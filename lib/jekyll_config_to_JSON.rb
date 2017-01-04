41638f0b2844874801b85841c57bdd94e9de4ee5
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-04 01:41:42 -0500

    finally fixed. regenerates fine now

--- lib/jekyll_config_to_JSON.rb
+++ lib/jekyll_config_to_JSON.rb
@@ -6,11 +6,11 @@ module Jekyll
     safe true
     priority :low
     @@globals = {
-      "json_file_path" => nil,
-      "output_directory" => "/api/v1/",
+      "json_file_path" => "",
+      "output_directory" => "/.api/v1/",
       "output_file" => "config.json",
-      "src_dir" => nil,
-      "dst_dir" => nil
+      "src_dir" => "",
+      "dst_dir" => ""
     }
     def self.json_output_directory
       @@globals["output_directory"]
@@ -18,26 +18,28 @@ module Jekyll
     def self.json_file
       @@globals["json_file_path"]
     end
-
+  
     def generate(site)
-       
-      @@globals["src_dir"] = File.join(site.source, @@globals["output_directory"]) unless !@@globals["src_dir"].nil?
-      @@globals["dst_dir"] = File.join(site.dest, @@globals["output_directory"]) unless !@@globals["dst_dir"].nil?
-      FileUtils.mkdir_p(@@globals["src_dir"]) unless File.exists?(@@globals["src_dir"])
-
       config = site.config['react']
       config_json = config.to_json
-      
-      @@globals["json_file_path"] = File.join(@@globals["src_dir"], @@globals["json_file"]) unless !@@globals["json_file_path"].nil?
-      f = File.new(@@globals["json_file_path"], "w+") unless !File.file(@@globals["json_file_path"])
-      f.puts config_json
-      f.close
-      if File.file?(src_dir)
-        site.static_files << Jekyll::StaticFile.new(site, site.source, @@globals['output_directory'], @@globals['output_file'])
+
+      if @@globals["src_dir"].to_s.empty? && @@globals["dst_dir"].to_s.empty?
+        @@globals["src_dir"] = File.join(site.source, @@globals["output_directory"])
+        @@globals["dst_dir"] = File.join(site.dest, @@globals["output_directory"])
+        FileUtils.mkdir_p(@@globals["src_dir"]) unless File.exists?(@@globals["src_dir"])
+        @@globals["json_file_path"] = File.join(@@globals["src_dir"], @@globals["output_file"])
+        f = File.new(@@globals["json_file_path"], "w+")
+        f.puts config_json
+        f.close
+        if File.exists?@@globals["json_file_path"]
+          site.static_files << Jekyll::StaticFile.new(site, site.source, @@globals['output_directory'], @@globals['output_file'])
+        end
       end
+
      
     end
   end
+
   #static override of the Jekyll Class
   class Site
     alias :super_write :write
@@ -45,16 +47,17 @@ module Jekyll
       # call the super method to generate our site
       # Must call otherwise our site will be gone :'(
       super_write
-
+      
       # cleanup source folder
-      src_folder = File.join(source, GenerateJSON::json_output_directory) unless !File.file?([source, GenerateJSON::json_output_directory].join('/'))
+      src_folder = File.join(source, GenerateJSON::json_output_directory)
       File.unlink GenerateJSON::json_file if File.exists?(GenerateJSON::json_file)
-      FileUtils.rm_rf(src_folder) unless !File.file?(src_folder)
-      src_arr = src_folder.split("/")
-      src_arr.pop
-      src_folder = src_arr.join("/")
-      FileUtils.rm_rf(src_folder) unless !File.file?(src_folder)
-
+      if File.exists?(src_folder)
+        FileUtils.rm_rf(src_folder)
+        src_arr = src_folder.split("/")
+        src_arr.pop
+        src_folder = src_arr.join("/")
+        FileUtils.rm_rf(src_folder)
+      end
     end
   end
 end

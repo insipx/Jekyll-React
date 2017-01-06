3ea371c7b0e44214ab04ad84672ad12e165482e3
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-06 17:26:23 -0500

    major changes, much more elegant solution

--- lib/jekyll_config_to_JSON.rb
+++ lib/jekyll_config_to_JSON.rb
@@ -1,8 +1,17 @@
+require 'json'
+require 'fileutils'
+
 module Jekyll
-  require 'json'
-  require 'fileutils'
- 
+  
+  # https://github.com/jekyll/jekyll-sitemap/blob/v0.7.0/lib/jekyll-sitemap.rb#L3-L8
+  class PageWithoutAFile < Page
+    def read_yaml(*)
+      @data ||= {}
+    end
+  end
+  
   class GenerateJSON < Jekyll::Generator
+    attr_reader :site
     safe true
     priority :low
     @@globals = {
@@ -12,41 +21,68 @@ module Jekyll
       "src_dir" => "",
       "dst_dir" => ""
     }
+
+    def generate(site)
+      @site = site
+      jobs
+    end
+
+    def initialize(site)
+      @site = site
+    end
+
     def self.json_output_directory
       @@globals["output_directory"]
     end
+
     def self.json_file
       @@globals["json_file_path"]
     end
-  
-    def generate(site)
-      config = site.config['react']
-      config_json = config.to_json
 
-      if @@globals["src_dir"].to_s.empty? && @@globals["dst_dir"].to_s.empty?
-        @@globals["src_dir"] = File.join(site.source, @@globals["output_directory"])
-        @@globals["dst_dir"] = File.join(site.dest, @@globals["output_directory"])
-        FileUtils.mkdir_p(@@globals["src_dir"]) unless File.exists?(@@globals["src_dir"])
-        @@globals["json_file_path"] = File.join(@@globals["src_dir"], @@globals["output_file"])
-        f = File.new(@@globals["json_file_path"], "w+")
-        f.puts config_json
-        f.close
-        if File.exists?@@globals["json_file_path"]
-          site.static_files << Jekyll::StaticFile.new(site, site.source, @@globals['output_directory'], @@globals['output_file'])
-        end
-      else
-        #else the filejoins have already been executed (IE: We are regenerating and not building)
-        FileUtils.mkdir_p(@@globals["src_dir"]) unless File.exists?(@@globals["src_dir"])
-        f = File.new(@@globals["json_file_path"], "w+")
-        f.puts config_json
-        f.close
-        if File.exists?@@globals["json_file_path"]
-          site.static_files << Jekyll::StaticFile.new(site, site.source, @@globals['output_directory'], @@globals['output_file'])
+    # write to file
+    def write(file, content)
+      f = File.new(file, "w+")
+      f.puts content
+      f.close
+    end
+    
+    def page(name, data)
+      page = PageWithoutAFile.new(@site, File.dirname(__FILE__), @@globals["output_directory"], name)
+      page.output = data.to_json
+      page
+    end
+
+    # add file to list of objects to be generated
+    def static_gen(output_dir, output_file)
+      if File.exists?@@globals["json_file_path"]
+        site.static_files << Jekyll::StaticFile.new(self.site, self.site.source, output_dir, output_file)
+      end
+    end
+
+    def gen_alt(name, data)
+      self.site.pages << self.page(name, data)
+    end
+
+    def jobs
+      config = @site.config['react']
+      config_json = config.to_json
+       
+      @site.pages.each do |page|
+        if page.data['render'] && page.data['render'] == 'dynamic'
+          puts page.url
+          page.content = ''
         end
       end
 
-
-     
+      if @@globals["src_dir"].to_s.empty? && @@globals["dst_dir"].to_s.empty?
+        @@globals["src_dir"] = File.join(@site.source, @@globals["output_directory"])
+        @@globals["dst_dir"] = File.join(@site.dest, @@globals["output_directory"])
+        @@globals["json_file_path"] = File.join(@@globals["src_dir"], @@globals["output_file"])
+        gen_alt(@@globals["output_file"], config_json)
+      else
+        #else the filejoins have already been executed (IE: We are regenerating and not building)
+        gen_alt(@@globals["output_file"], config_json)
+      end
     end
   end
 
@@ -57,7 +93,7 @@ module Jekyll
       # call the super method to generate our site
       # Must call otherwise our site will be gone :'(
       super_write
-      
+=begin 
       # cleanup source folder
       src_folder = File.join(source, GenerateJSON::json_output_directory)
       File.unlink GenerateJSON::json_file if File.exists?(GenerateJSON::json_file)
@@ -68,6 +104,7 @@ module Jekyll
         src_folder = src_arr.join("/")
         FileUtils.rm_rf(src_folder)
       end
+=end
     end
   end
 end

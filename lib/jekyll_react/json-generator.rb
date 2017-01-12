76aacfa70afdcc1f5c8d484733c7e7da3cb0b574
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-11 21:00:41 -0500

    new update to take _data files and spit them out as json

--- lib/jekyll_react/json-generator.rb
+++ lib/jekyll_react/json-generator.rb
@@ -24,6 +24,7 @@ module Jekyll
       config_json = config.to_json
       new_page('config.json', config_json, self.dest_dir)
       parse_pages
+      parse_data
     end
     
     def parse_pages
@@ -37,6 +38,12 @@ module Jekyll
           new_page(name, page_json, page.url)
           page.content = ''
         end
+      end
+    end
+    
+    def parse_data
+      @site.data.each do |site_file|
+        new_page("#{site_file[0]}.json", site_file.to_json, self.dest_dir)
       end
     end
 
